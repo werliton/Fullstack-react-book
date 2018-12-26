5c49a2d23a8c43f4d234c3608ef7dcbb3c9ab2fd
werliton <werlitoncarlos@gmail.com>
2018-12-26 08:28:39 -0300

    Creating first component

---
+++ src/component/ProductList.js
@@ -0,0 +1,12 @@
+import React, { Component } from 'react'
+
+
+export default class ProductList extends Component{
+    render() {
+      return (
+        <div className="ui unstackable items">
+          Hello, friend! I am a basic React component.
+        </div>
+      )
+    }
+}
