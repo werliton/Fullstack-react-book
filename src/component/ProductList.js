5b2b0e31ae68624d80f30150e2af89c46aa9b8f5
werliton <werlitoncarlos@gmail.com>
2018-12-27 00:13:35 -0300

    Ordenando por votos

--- src/component/ProductList.js
+++ src/component/ProductList.js
@@ -4,25 +4,25 @@ import {data} from '../dados'
 
 export default class ProductList extends Component{
     render() {
-      const products = data;
+      const products = data.sort((a, b) => (
+        b.votes - a.votes
+      ));
+      const productComponents = products.map((product, key) => (
+          <Product 
+            key={key}
+              id={product.id}
+              title={product.title}
+              description={product.description}
+              url={product.url}
+              votes={product.votes}
+              submitterAvatarUrl={product.submitter_avatar_url}
+              productImageUrl={product.product_image_url}
+          />
+      ))
       
       return (
         <div className="ui unstackable items">
-        {
-          products.map((product, key) => (
-              <Product 
-                key={key}
-                  id={product.id}
-                  title={product.title}
-                  description={product.description}
-                  url={product.url}
-                  votes={product.votes}
-                  submitterAvatarUrl={product.submitter_avatar_url}
-                  productImageUrl={product.product_image_url}
-              />
-          ))
-        }
-          
+          {productComponents}          
         </div>
       )
     }
